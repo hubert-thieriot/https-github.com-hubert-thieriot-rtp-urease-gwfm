3111b6f6b0e72021a1cf35be7ae3c86a8c05e339
hubert-crea <hubert@energyandcleanair.org>
2024-06-03 14:06:10 +0100

    -first commit comtrade

---
+++ comtrade.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+remotes::install_github("ropensci/comtradr")
+library(comtradr)
+library(summarytools)
+
+readRenviron(".Renviron")
+HSCODE_UREA <- "310210"
+
+
+# Get data
+comtrade_data <- comtradr::ct_get_data(
+  commodity_code = HSCODE_UREA,
+  start_date = 2023,
+  end_date = 2023
+)
+
+# Check data
+summarytools::dfSummary(comtrade_data)
+
+# Format for GWFM
+comtrade_data %>%
+  group_by(period, iso3=reporter_iso, country=reporter_desc, flow=flow_desc) %>%
+  summarise(net_wgt=sum(net_wgt)) %>%
+  tidyr::spread(flow, net_wgt, fill = 0) %>%
+  mutate(net_export = Export + `Re-export` - Import - `Re-import`) %>%
+  ungroup() %>%
+  select(country, net_export) %>%
+  mutate(unit = 'kg', year=2023, hs_code=HSCODE_UREA) %>%
+  arrange(desc(net_export)) %>%
+  clipr::write_clip()
