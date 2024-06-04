1c9aaf88fc9be7305dc3a6b3c06087c548fbee22
hubert-crea <hubert@energyandcleanair.org>
2024-06-04 15:26:02 +0100

    -first commit faolex

---
+++ faolex.R
@@ -0,0 +1,33 @@
+# Extracting number of relevant regulations from https://www.nature.com/articles/s41893-020-0577-7#Sec18
+# The authors of the paper stored the data in a Google Sheet: https://docs.google.com/spreadsheets/d/1hOfl5Np80oC4EXrNMi7emnhx3RByRFSvOfEr9f2GJC4/edit#gid=0
+
+
+library(tidyverse)
+
+# Load the data
+url <- "https://docs.google.com/spreadsheets/d/1hOfl5Np80oC4EXrNMi7emnhx3RByRFSvOfEr9f2GJC4/export?format=csv"
+regulations <- readr::read_csv(url)
+names(regulations)
+
+count <- regulations %>%
+  dplyr::select(country=Country,
+           descriptors=Descriptors,
+           sector=Sector,
+           scale=Scale) %>%
+  filter(grepl("fertili", descriptors, ignore.case = TRUE),
+         scale=="National",
+         sector=="Agriculture") %>%
+  group_by(country) %>%
+  summarise(count=n()) %>%
+  arrange(desc(count))
+
+# Export
+count %>%
+  head(50) %>%
+  ggplot() +
+  geom_bar(aes(count, reorder(country, count)), stat = "identity")
+
+
+count %>%
+  dplyr::select(country, count) %>%
+  clipr::write_clip()
